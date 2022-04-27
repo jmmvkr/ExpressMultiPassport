4bd857bf365c2e069c62041eae26846d72729d15
jmmvkr <jmmvkr@gmail.com>
2022-04-27 17:04:53 +0800

    Fix minor formatting problem

--- global.js
+++ global.js
@@ -3,50 +3,49 @@
  * 
  * @see global
  */
- class Global { }
+class Global { }
 
 
- /**
-  * A message object is assumed to be a normal Object or an Error, with string property named 'message'
-  * @typedef {Object|Error} MessageObject
-  * @property {string} message - Message stored in the message object
-  */
- 
- /**
- * Indicates that a parameter accepts all types of input value
- * @typedef {Object|string|number|boolean} AnyType
+/**
+ * A message object is assumed to be a normal Object or an Error, with string property named 'message'
+ * @typedef {Object|Error} MessageObject
+ * @property {string} message - Message stored in the message object
  */
- 
- /**
-  * A router type of an Express.js site.
-  * @see {@link https://expressjs.com/en/guide/routing.html|Express.js Router}
-  * @typedef {express.Router} Router
-  */
- 
- /**
-  * An HTTP request in Express.js
-  * @see {@link https://expressjs.com/en/4x/api.html#req|Express.js Request}
-  * @typedef {object} Request
-  */
- 
- /**
-  * An HTTP response in Express.js
-  * @see {@link https://expressjs.com/en/4x/api.html#res|Express.js Response}
-  * @typedef {object} Response
-  */
- 
- /**
-  * Callback of next Express.js middleware.
-  * @callback NextCallback
-  */
- 
- /**
-  * A namespace for routes.
-  * @namespace routes
-  */
 
-  /**
-  * A namespace for DAL (Data Access Level) classes.
-  * @namespace dal
-  */
- 
+/**
+* Indicates that a parameter accepts all types of input value
+* @typedef {Object|string|number|boolean} AnyType
+*/
+
+/**
+ * A router type of an Express.js site.
+ * @see {@link https://expressjs.com/en/guide/routing.html|Express.js Router}
+ * @typedef {express.Router} Router
+ */
+
+/**
+ * An HTTP request in Express.js
+ * @see {@link https://expressjs.com/en/4x/api.html#req|Express.js Request}
+ * @typedef {object} Request
+ */
+
+/**
+ * An HTTP response in Express.js
+ * @see {@link https://expressjs.com/en/4x/api.html#res|Express.js Response}
+ * @typedef {object} Response
+ */
+
+/**
+ * Callback of next Express.js middleware.
+ * @callback NextCallback
+ */
+
+/**
+ * A namespace for routes.
+ * @namespace routes
+ */
+
+/**
+* A namespace for DAL (Data Access Level) classes.
+* @namespace dal
+*/

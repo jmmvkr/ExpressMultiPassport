efb7c34e149dc407ce6c904d351d956f1f66baaa
jmmvkr <jmmvkr@gmail.com>
2022-04-27 16:41:21 +0800

    Add global types to make document more clear

---
+++ global.js
@@ -0,0 +1,52 @@
+/**
+ * Place holder class in order to have a link to {@link global|global types}.
+ * 
+ * @see global
+ */
+ class Global { }
+
+
+ /**
+  * A message object is assumed to be a normal Object or an Error, with string property named 'message'
+  * @typedef {Object|Error} MessageObject
+  * @property {string} message - Message stored in the message object
+  */
+ 
+ /**
+ * Indicates that a parameter accepts all types of input value
+ * @typedef {Object|string|number|boolean} AnyType
+ */
+ 
+ /**
+  * A router type of an Express.js site.
+  * @see {@link https://expressjs.com/en/guide/routing.html|Express.js Router}
+  * @typedef {express.Router} Router
+  */
+ 
+ /**
+  * An HTTP request in Express.js
+  * @see {@link https://expressjs.com/en/4x/api.html#req|Express.js Request}
+  * @typedef {object} Request
+  */
+ 
+ /**
+  * An HTTP response in Express.js
+  * @see {@link https://expressjs.com/en/4x/api.html#res|Express.js Response}
+  * @typedef {object} Response
+  */
+ 
+ /**
+  * Callback of next Express.js middleware.
+  * @callback NextCallback
+  */
+ 
+ /**
+  * A namespace for routes.
+  * @namespace routes
+  */
+
+  /**
+  * A namespace for DAL (Data Access Level) classes.
+  * @namespace dal
+  */
+ 

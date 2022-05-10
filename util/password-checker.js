086ae60abd559b8bf35020207448f1bd93a3bd4d
jmmvkr <jmmvkr@gmail.com>
2022-05-10 10:38:08 +0800

    In *.js, replace tab (\t) with spaces

--- util/password-checker.js
+++ util/password-checker.js
@@ -10,23 +10,23 @@ const RX_SPECIAL = /[\/!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/;
  */
 const InvalidPassword = {
 
-	/** @description Missing lower alphabet in password */
-	PASSWORD_NO_LOWER_CHAR: 1, 
+    /** @description Missing lower alphabet in password */
+    PASSWORD_NO_LOWER_CHAR: 1,
 
-	/** @description Missing upper alphabet in password */
-	PASSWORD_NO_UPPER_CHAR: 2, 
+    /** @description Missing upper alphabet in password */
+    PASSWORD_NO_UPPER_CHAR: 2,
 
-	/** @description Missing digit in password */
-	PASSWORD_NO_DIGIT_CHAR: 3, 
+    /** @description Missing digit in password */
+    PASSWORD_NO_DIGIT_CHAR: 3,
 
-	/** @description Missing symbol in password */
-	PASSWORD_NO_SYMBOL_CHAR: 4, 
+    /** @description Missing symbol in password */
+    PASSWORD_NO_SYMBOL_CHAR: 4,
 
-	/** @description Password is too short */
-	PASSWORD_TOO_SHORT: 5, 
+    /** @description Password is too short */
+    PASSWORD_TOO_SHORT: 5,
 
-	/** @description Passwords (first, and confirmed) provided are different */
-	PASSWORD_CONFIRM_NOT_SAME: 6,
+    /** @description Passwords (first, and confirmed) provided are different */
+    PASSWORD_CONFIRM_NOT_SAME: 6,
 };
 
 /**
@@ -49,170 +49,169 @@ const InvalidPassword = {
  */
 class PasswordChecker {
 
-/**
- * Create a PasswordChecker instance with given options.
- * 
- * @param {Object} options - Options to be applied to created instance
- */
-constructor(options) {
-	const instanceOptions = options || {};
+    /**
+     * Create a PasswordChecker instance with given options.
+     * 
+     * @param {Object} options - Options to be applied to created instance
+     */
+    constructor(options) {
+        const instanceOptions = options || {};
+
+        /** 
+         * @member {number} - Minimum accepted password length
+         * @default 8
+         */
+        this.strongLength = instanceOptions.strongLength || 8;
+
+        /** 
+         * @member {boolean} - When set to true, generates sample (human-frendly) message when checkPassword() failed 
+         * @default false
+         */
+        this.enableSampleMessage = (true === instanceOptions.enableSampleMessage);
+    }
+
+    /**
+     * Check given password input has a lower case character
+     * @param {string} passwordInput - The password input
+     * @return {boolean} - True if passwordInput has a lower case character
+     */
+    hasLowerCharacter(passwordInput) {
+        return RX_LOWER.test(passwordInput);
+    }
+
+    /**
+     * Check given password input has a upper case character
+     * @param {string} passwordInput - The password input
+     * @return {boolean} - True if passwordInput has a upper case character
+     */
+    hasUpperCharacter(passwordInput) {
+        return RX_UPPER.test(passwordInput);
+    }
+
+    /**
+     * Check given password input has a digit character
+     * @param {string} passwordInput - The password input
+     * @return {boolean} - True if passwordInput has a digit character
+     */
+    hasDigitCharacter(passwordInput) {
+        return RX_DIGIT.test(passwordInput);
+    }
+
+    /**
+     * Check given password input has a special character
+     * @param {string} passwordInput - The password input
+     * @return {boolean} - True if passwordInput has a special character
+     */
+    hasSpecialCharacter(passwordInput) {
+        return RX_SPECIAL.test(passwordInput);
+    }
+
+    /**
+     * Check given password input has strong length
+     * @param {string} passwordInput - The password input
+     * @return {boolean} - True if passwordInput has strong length
+     * @see #strongLength
+     */
+    hasStrongLength(passwordInput) {
+        return (passwordInput.length >= this.strongLength);
+    }
+
+    /**
+     * Check given password input is a valid password or not.
+     * @param {string} passwordInput - The password input
+     * @param {string} [passwordConfirm] - The password confirm input
+     * @return {InputCheck} - An InputCheck instance, describing the result of input checking
+     */
+    checkPassword(passwordInput, passwordConfirm) {
+        var invalidNotes = [];
+        this.collectInvalidNotes(invalidNotes, this.hasLowerCharacter(passwordInput), InvalidPassword.PASSWORD_NO_LOWER_CHAR);
+        this.collectInvalidNotes(invalidNotes, this.hasUpperCharacter(passwordInput), InvalidPassword.PASSWORD_NO_UPPER_CHAR);
+        this.collectInvalidNotes(invalidNotes, this.hasDigitCharacter(passwordInput), InvalidPassword.PASSWORD_NO_DIGIT_CHAR);
+        this.collectInvalidNotes(invalidNotes, this.hasSpecialCharacter(passwordInput), InvalidPassword.PASSWORD_NO_SYMBOL_CHAR);
+        this.collectInvalidNotes(invalidNotes, this.hasStrongLength(passwordInput), InvalidPassword.PASSWORD_TOO_SHORT);
+
+        var samePassword;
+        if ((passwordConfirm) && (0 === invalidNotes.length)) {
+            samePassword = (passwordConfirm === passwordInput);
+            this.collectInvalidNotes(invalidNotes, samePassword, InvalidPassword.PASSWORD_CONFIRM_NOT_SAME);
+        }
+
+        const isValid = (0 === invalidNotes.length);
+        const result = {
+            input: passwordInput,
+            isValid,
+            invalidNotes
+        }
+        return result;
+    }
 
     /** 
-	 * @member {number} - Minimum accepted password length
-	 * @default 8
-	 */
-	this.strongLength = instanceOptions.strongLength || 8;
+     * Collect validation violation note if input is invalid
+     * @private
+     * @param {InvalidNote[]} invalidNotes - Array to collect invalid input note
+     * @param {boolean} isValid - Validation check passed from caller
+     * @param {number} noteIn - The validation violation note (numeric) passed from caller
+     */
+    collectInvalidNotes(invalidNotes, isValid, noteIn) {
+        var messageIn = '';
+        if (!isValid) {
+            if (this.enableSampleMessage) {
+                messageIn = this.getSampleMessage(noteIn);
+                invalidNotes.push({
+                    note: noteIn,
+                    message: messageIn,
+                });
+            } else {
+                invalidNotes.push({
+                    note: noteIn
+                });
+            }
+        }
+    }
 
     /** 
-	 * @member {boolean} - When set to true, generates sample (human-frendly) message when checkPassword() failed 
-	 * @default false
-	 */
-	this.enableSampleMessage = (true === instanceOptions.enableSampleMessage);
-}
+     * Generates sample messages for internal debug
+     * @private
+     * @param {number} invalidNote - The validation violation note (numeric) to generate message
+     */
+    getSampleMessage(invalidNote) {
+        switch (invalidNote) {
+            case InvalidPassword.PASSWORD_NO_LOWER_CHAR:
+                return 'Password must contain at least one lower character';
+            case InvalidPassword.PASSWORD_NO_UPPER_CHAR:
+                return 'Password must contain at least one upper character';
+            case InvalidPassword.PASSWORD_NO_DIGIT_CHAR:
+                return 'Password must contain at least one digit character';
+            case InvalidPassword.PASSWORD_NO_SYMBOL_CHAR:
+                return 'Password must contain at least one special character';
+            case InvalidPassword.PASSWORD_TOO_SHORT:
+                return 'Password must contain at least ' + this.strongLength + ' characters';
+            case InvalidPassword.PASSWORD_CONFIRM_NOT_SAME:
+                return 'Password confirm must be same as the first password input';
+        }
+        return 'unknown password error';
+    }
 
-/**
- * Check given password input has a lower case character
- * @param {string} passwordInput - The password input
- * @return {boolean} - True if passwordInput has a lower case character
- */
-hasLowerCharacter(passwordInput) {
-	return RX_LOWER.test(passwordInput);
-}
+    static runSample() {
+        const options = { enableSampleMessage: true };
+        const passwordChecker = new PasswordChecker(options);
 
-/**
- * Check given password input has a upper case character
- * @param {string} passwordInput - The password input
- * @return {boolean} - True if passwordInput has a upper case character
- */
-hasUpperCharacter(passwordInput) {
-	return RX_UPPER.test(passwordInput);
-}
-
-/**
- * Check given password input has a digit character
- * @param {string} passwordInput - The password input
- * @return {boolean} - True if passwordInput has a digit character
- */
-hasDigitCharacter(passwordInput) {
-	return RX_DIGIT.test(passwordInput);
-}
-
-/**
- * Check given password input has a special character
- * @param {string} passwordInput - The password input
- * @return {boolean} - True if passwordInput has a special character
- */
-hasSpecialCharacter(passwordInput) {
-	return RX_SPECIAL.test(passwordInput);
-}
-
-/**
- * Check given password input has strong length
- * @param {string} passwordInput - The password input
- * @return {boolean} - True if passwordInput has strong length
- * @see #strongLength
- */
-hasStrongLength(passwordInput) {
-	return (passwordInput.length >= this.strongLength);
-}
-
-/**
- * Check given password input is a valid password or not.
- * @param {string} passwordInput - The password input
- * @param {string} [passwordConfirm] - The password confirm input
- * @return {InputCheck} - An InputCheck instance, describing the result of input checking
- */
-checkPassword(passwordInput, passwordConfirm) {
-	var invalidNotes = [];
-	this.collectInvalidNotes(invalidNotes, this.hasLowerCharacter(passwordInput), InvalidPassword.PASSWORD_NO_LOWER_CHAR);
-	this.collectInvalidNotes(invalidNotes, this.hasUpperCharacter(passwordInput), InvalidPassword.PASSWORD_NO_UPPER_CHAR);
-	this.collectInvalidNotes(invalidNotes, this.hasDigitCharacter(passwordInput), InvalidPassword.PASSWORD_NO_DIGIT_CHAR);
-	this.collectInvalidNotes(invalidNotes, this.hasSpecialCharacter(passwordInput), InvalidPassword.PASSWORD_NO_SYMBOL_CHAR);
-	this.collectInvalidNotes(invalidNotes, this.hasStrongLength(passwordInput), InvalidPassword.PASSWORD_TOO_SHORT);
-	
-	var samePassword;
-	if((passwordConfirm) && (0 === invalidNotes.length)) {
-		samePassword = (passwordConfirm === passwordInput);
-		this.collectInvalidNotes(invalidNotes, samePassword, InvalidPassword.PASSWORD_CONFIRM_NOT_SAME);
-	}
-	
-	const isValid = (0 === invalidNotes.length);
-	const result = {
-		input: passwordInput, 
-		isValid, 
-		invalidNotes
-	}
-	return result;
-}
-
-/** 
- * Collect validation violation note if input is invalid
- * @private
- * @param {InvalidNote[]} invalidNotes - Array to collect invalid input note
- * @param {boolean} isValid - Validation check passed from caller
- * @param {number} noteIn - The validation violation note (numeric) passed from caller
- */
-collectInvalidNotes(invalidNotes, isValid, noteIn) {
-	var messageIn = '';
-	if(! isValid) {
-		if(this.enableSampleMessage) {
-			messageIn = this.getSampleMessage(noteIn);
-			invalidNotes.push({
-				note: noteIn, 
-				message: messageIn, 
-			});
-		} else {
-			invalidNotes.push({
-				note: noteIn
-			});
-		}
-	}
-}
-
-/** 
- * Generates sample messages for internal debug
- * @private
- * @param {number} invalidNote - The validation violation note (numeric) to generate message
- */
-getSampleMessage(invalidNote) {
-	switch(invalidNote)
-	{
-		case InvalidPassword.PASSWORD_NO_LOWER_CHAR:
-			return 'Password must contain at least one lower character';
-		case InvalidPassword.PASSWORD_NO_UPPER_CHAR:
-			return 'Password must contain at least one upper character';
-		case InvalidPassword.PASSWORD_NO_DIGIT_CHAR:
-			return 'Password must contain at least one digit character';
-		case InvalidPassword.PASSWORD_NO_SYMBOL_CHAR:
-			return 'Password must contain at least one special character';
-		case InvalidPassword.PASSWORD_TOO_SHORT:
-			return 'Password must contain at least ' + this.strongLength + ' characters';
-		case InvalidPassword.PASSWORD_CONFIRM_NOT_SAME:
-			return 'Password confirm must be same as the first password input';
-	}
-	return 'unknown password error';
-}
-
-static runSample() {
-	const options = { enableSampleMessage: true };
-	const passwordChecker = new PasswordChecker(options);
-
-	console.log('----\r\n', passwordChecker.checkPassword(''));
-	console.log('----\r\n', passwordChecker.checkPassword('a'));
-	console.log('----\r\n', passwordChecker.checkPassword('aW'));
-	console.log('----\r\n', passwordChecker.checkPassword('+-345678'));
-	console.log('----\r\n', passwordChecker.checkPassword('aW5+'));
-	console.log('----\r\n', passwordChecker.checkPassword('aW5+tes'));
-	console.log('----\r\n', passwordChecker.checkPassword('aW5+test'));
-	console.log('----\r\n', passwordChecker.checkPassword('aW6+test', 'aW7+test'));
-	console.log('----\r\n', passwordChecker.checkPassword('aW7+test', 'aW7+test'));
-	console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_LOWER_CHAR));
-	console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_UPPER_CHAR));
-	console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_DIGIT_CHAR));
-	console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_SYMBOL_CHAR));
-	console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_TOO_SHORT));
-	console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_CONFIRM_NOT_SAME));
-}
+        console.log('----\r\n', passwordChecker.checkPassword(''));
+        console.log('----\r\n', passwordChecker.checkPassword('a'));
+        console.log('----\r\n', passwordChecker.checkPassword('aW'));
+        console.log('----\r\n', passwordChecker.checkPassword('+-345678'));
+        console.log('----\r\n', passwordChecker.checkPassword('aW5+'));
+        console.log('----\r\n', passwordChecker.checkPassword('aW5+tes'));
+        console.log('----\r\n', passwordChecker.checkPassword('aW5+test'));
+        console.log('----\r\n', passwordChecker.checkPassword('aW6+test', 'aW7+test'));
+        console.log('----\r\n', passwordChecker.checkPassword('aW7+test', 'aW7+test'));
+        console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_LOWER_CHAR));
+        console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_UPPER_CHAR));
+        console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_DIGIT_CHAR));
+        console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_NO_SYMBOL_CHAR));
+        console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_TOO_SHORT));
+        console.log(passwordChecker.getSampleMessage(InvalidPassword.PASSWORD_CONFIRM_NOT_SAME));
+    }
 
 
 } // end - class
--- views/f/js/oauth.js
+++ views/f/js/oauth.js
@@ -1,230 +1,230 @@
 class OAuthSuite {
-	
-	constructor(suiteName) {
-		var noOp = function() {};
-		this.name = suiteName;
-		this.triggerSignIn = noOp;
-	}
-	
-	static postJson(uri, obj) {
-		var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
-		xmlhttp.open("POST", uri);
-		xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-		xmlhttp.onreadystatechange = function () {
-			if (xmlhttp.readyState === XMLHttpRequest.DONE) {
-				console.log('response: ', xmlhttp.responseText)
-			}
-		}
-		xmlhttp.send(JSON.stringify(obj));
-	}
-	
-	static makeGoogleInstance() {
-		var suite = new OAuthSuite('google');
-		suite.triggerSignIn = () => {
-			document.querySelector('.g-signin2').querySelector('.abcRioButton').click();
-		};
-		suite.onSignIn = (googleUser) => {
-			var profile = googleUser.getBasicProfile();
 
-			console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-			console.log('Name: ' + profile.getName());
-			console.log('Image URL: ' + profile.getImageUrl());
-			console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    constructor(suiteName) {
+        var noOp = function () { };
+        this.name = suiteName;
+        this.triggerSignIn = noOp;
+    }
 
-			var id_token = googleUser.getAuthResponse().id_token;
-			console.log('id_token: ' + id_token);
+    static postJson(uri, obj) {
+        var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+        xmlhttp.open("POST", uri);
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+                console.log('response: ', xmlhttp.responseText)
+            }
+        }
+        xmlhttp.send(JSON.stringify(obj));
+    }
 
-			var signinObj = {
-				cat: 'google', 
-				email: profile.getEmail(), 
-				token: id_token, 
-			}
-			OAuthSuite.postJson('/api/signin', signinObj);
-		};
-		return suite;
-	}
-	
-	static makeOauthPreset() {
-		var authMap = {
-			google: OAuthSuite.makeGoogleInstance()
-		}
-		return authMap;
-	}
+    static makeGoogleInstance() {
+        var suite = new OAuthSuite('google');
+        suite.triggerSignIn = () => {
+            document.querySelector('.g-signin2').querySelector('.abcRioButton').click();
+        };
+        suite.onSignIn = (googleUser) => {
+            var profile = googleUser.getBasicProfile();
+
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log('id_token: ' + id_token);
+
+            var signinObj = {
+                cat: 'google',
+                email: profile.getEmail(),
+                token: id_token,
+            }
+            OAuthSuite.postJson('/api/signin', signinObj);
+        };
+        return suite;
+    }
+
+    static makeOauthPreset() {
+        var authMap = {
+            google: OAuthSuite.makeGoogleInstance()
+        }
+        return authMap;
+    }
 }
 
 function oauthGoogleSignIn() {
-	oauthPreset.google.triggerSignIn();
+    oauthPreset.google.triggerSignIn();
 }
 
 function onGoogleSignIn(ev) {
-	oauthPreset.google.onSignIn(ev);
+    oauthPreset.google.onSignIn(ev);
 }
 
 function oauthFacebookSignIn() {
-	console.info('not implemented');
+    console.info('not implemented');
 }
 
 function displayError(message) {
-	var ele = document.querySelector('.login-error');
-	ele.classList.remove('box-hidden');
-	ele.innerHTML = message.replaceAll('\\n', '\n').replace(/\n/g, '<br/>');
-	return false;
+    var ele = document.querySelector('.login-error');
+    ele.classList.remove('box-hidden');
+    ele.innerHTML = message.replaceAll('\\n', '\n').replace(/\n/g, '<br/>');
+    return false;
 }
 
 function displayInformation(message) {
-	var ele = document.querySelector('.login-information');
-	ele.classList.remove('box-hidden');
-	ele.innerHTML = message.replaceAll('\\n', '\n').replace(/\n/g, '<br/>');
-	setTimeout(function() {
-		ele.classList.add('box-hidden');
-	}, 3000);
-	return false;
+    var ele = document.querySelector('.login-information');
+    ele.classList.remove('box-hidden');
+    ele.innerHTML = message.replaceAll('\\n', '\n').replace(/\n/g, '<br/>');
+    setTimeout(function () {
+        ele.classList.add('box-hidden');
+    }, 3000);
+    return false;
 }
 
 function showUiCell(target, isVisibleLater) {
-	var ele = (target);
-	if(target.constructor === String) {
-		ele = document.querySelector(target);
-	}
-	
-	if(isVisibleLater) {
-		ele.classList.remove('box-hidden');
-	} else {
-		ele.classList.add('box-hidden');
-	}
+    var ele = (target);
+    if (target.constructor === String) {
+        ele = document.querySelector(target);
+    }
+
+    if (isVisibleLater) {
+        ele.classList.remove('box-hidden');
+    } else {
+        ele.classList.add('box-hidden');
+    }
 }
 
 function getCookie(cname) {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for(let i = 0; i <ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) === ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) === 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return "";
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 function fillEmail() {
-	var cookieEmail = getCookie('email');
-	if(cookieEmail) {
-		document.querySelector('#emailAddr').value = cookieEmail;
-	}
+    var cookieEmail = getCookie('email');
+    if (cookieEmail) {
+        document.querySelector('#emailAddr').value = cookieEmail;
+    }
 }
 
 function checkEmail(email) {
-	document.cookie = `email=${email};path=/`;
-	if(email.length <= 0) {
-		return displayError('Please enter E-mail');
-	}
-	return true;
+    document.cookie = `email=${email};path=/`;
+    if (email.length <= 0) {
+        return displayError('Please enter E-mail');
+    }
+    return true;
 }
 
 function checkPassword(password, message) {
-	if(password.length <= 0) {
-		return displayError(message || 'Please enter Password');
-	}
-	return true;
+    if (password.length <= 0) {
+        return displayError(message || 'Please enter Password');
+    }
+    return true;
 }
 
 function checkLogin() {
-	var email = document.querySelector('#emailAddr').value;
-	var password = document.querySelector('#password').value;
-	return checkEmail(email) && checkPassword(password);
+    var email = document.querySelector('#emailAddr').value;
+    var password = document.querySelector('#password').value;
+    return checkEmail(email) && checkPassword(password);
 }
 
 function checkSignUp() {
-	var email = document.querySelector('#emailAddr').value;
-	var password = document.querySelector('#password').value;
-	var passwordConfirm = document.querySelector('#passwordConfirm').value;
-	if(false === (checkEmail(email) && checkPassword(password))) {
-		return false;
-	}
-	if(password === passwordConfirm) {
-		return true;
-	}
-	return displayError('Confirm Password not match');
+    var email = document.querySelector('#emailAddr').value;
+    var password = document.querySelector('#password').value;
+    var passwordConfirm = document.querySelector('#passwordConfirm').value;
+    if (false === (checkEmail(email) && checkPassword(password))) {
+        return false;
+    }
+    if (password === passwordConfirm) {
+        return true;
+    }
+    return displayError('Confirm Password not match');
 }
 
 function checkPasswordChange() {
-	var oldPassword = document.querySelector('#oldPassword').value;
-	var newPassword = document.querySelector('#password').value;
-	var passwordConfirm = document.querySelector('#passwordConfirm').value;
-	
-	if(! checkPassword(oldPassword, 'Please enter Old Password')) {
-		return false;
-	}
-	if(! checkPassword(newPassword, 'Please enter New Password')) {
-		return false;
-	}
-	if(! checkPassword(passwordConfirm, 'Please enter Confirm New Password')) {
-		return false;
-	}
-	if(newPassword === passwordConfirm) {
-		return true;
-	}
-	return displayError('Confirm Password not match');
+    var oldPassword = document.querySelector('#oldPassword').value;
+    var newPassword = document.querySelector('#password').value;
+    var passwordConfirm = document.querySelector('#passwordConfirm').value;
+
+    if (!checkPassword(oldPassword, 'Please enter Old Password')) {
+        return false;
+    }
+    if (!checkPassword(newPassword, 'Please enter New Password')) {
+        return false;
+    }
+    if (!checkPassword(passwordConfirm, 'Please enter Confirm New Password')) {
+        return false;
+    }
+    if (newPassword === passwordConfirm) {
+        return true;
+    }
+    return displayError('Confirm Password not match');
 }
 
 function postJson(uri, obj, cbText) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", uri);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			cbText(xhr.responseText);
-		}
-	}
-	xhr.send(JSON.stringify(obj));
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", uri);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            cbText(xhr.responseText);
+        }
+    }
+    xhr.send(JSON.stringify(obj));
 }
 
 function getJson(uri, cbText) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", uri);
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			cbText(xhr.responseText);
-		}
-	}
-	xhr.send(null);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", uri);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            cbText(xhr.responseText);
+        }
+    }
+    xhr.send(null);
 }
 
 function updatePassword() {
-	var isValid = checkPasswordChange();
-	
-	if(! isValid) {
-		return false;
-	}
-	
-	var userId = parseInt(document.querySelector('#userId').value);
-	var oldPassword = document.querySelector('#oldPassword').value;
-	var password = document.querySelector('#password').value;
-	
-	var obj = {
-		userId, 
-		oldPassword, 
-		password
-	};
-	
-	showUiCell('.login-error', false);
-	showUiCell('.login-information', false);
-	document.querySelector('#oldPassword').value = '';
-	document.querySelector('#password').value = '';
-	document.querySelector('#passwordConfirm').value = '';
-	
-	postJson('/user/reset-password', obj, (text) => {
-		var result = JSON.parse(text);
-		if(result.message) {
-			return displayError(result.message);
-		}
-		if(result.information) {
-			return displayInformation(result.information);
-		}
-	});
-	return false;
+    var isValid = checkPasswordChange();
+
+    if (!isValid) {
+        return false;
+    }
+
+    var userId = parseInt(document.querySelector('#userId').value);
+    var oldPassword = document.querySelector('#oldPassword').value;
+    var password = document.querySelector('#password').value;
+
+    var obj = {
+        userId,
+        oldPassword,
+        password
+    };
+
+    showUiCell('.login-error', false);
+    showUiCell('.login-information', false);
+    document.querySelector('#oldPassword').value = '';
+    document.querySelector('#password').value = '';
+    document.querySelector('#passwordConfirm').value = '';
+
+    postJson('/user/reset-password', obj, (text) => {
+        var result = JSON.parse(text);
+        if (result.message) {
+            return displayError(result.message);
+        }
+        if (result.information) {
+            return displayInformation(result.information);
+        }
+    });
+    return false;
 }
